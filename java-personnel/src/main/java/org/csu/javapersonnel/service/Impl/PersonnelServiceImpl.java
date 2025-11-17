6f69ac3a40047cfc920607a8265fb5e362d6ee55
mysteryMisery <925826489@qq.com>
2025-11-17 14:57:04 +0800

    personnel bug修复

--- java-personnel/src/main/java/org/csu/javapersonnel/service/Impl/PersonnelServiceImpl.java
+++ java-personnel/src/main/java/org/csu/javapersonnel/service/Impl/PersonnelServiceImpl.java
@@ -6,7 +6,22 @@ import org.csu.javapersonnel.mapper.PersonnelMapper;
 import org.csu.javapersonnel.service.PersonnelService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class PersonnelServiceImpl extends ServiceImpl<PersonnelMapper, Personnel>
         implements PersonnelService
-{}
+{
+    @Override
+    public boolean save(Personnel p) {
+        p.setCreatedAt(LocalDateTime.now());
+        p.setUpdatedAt(LocalDateTime.now());
+        return super.save(p);
+    }
+
+    @Override
+    public boolean updateById(Personnel p) {
+        p.setUpdatedAt(LocalDateTime.now());
+        return super.updateById(p);
+    }
+}
